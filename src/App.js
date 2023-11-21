71d3462ec175f097aa1b433f46431e499f3e1bf3
cuneiform-umangrathod <umang.rathod@thecuneiform.com>
2023-11-21 13:54:30 +0530

    reducing more code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useFormik } from "formik";
 import "./App.css";
 import { useEffect, useState } from "react";
-
+import { data } from "./data";
 function App() {
   const initialValues = {
     num1: "",
@@ -18,10 +18,11 @@ function App() {
   });
 
   const [listArr, setListArr] = useState();
-  const [fieldName, setFieldName] = useState('num1');
+  const [fieldName, setFieldName] = useState("num1");
 
-
-  // handle Change function
+  /**
+   * * this fucntion handle change event
+   */
 
   const handleChange = (e) => {
     const checkNum = e.target.value;
@@ -51,54 +52,11 @@ function App() {
     str = listArr.join("");
 
     formik.setFieldValue(name, str);
-
-
-    // switch (name) {
-    //   case "num1":
-        
-
-    //     // let check3 = e.target.value;
-    //     // formik.setFieldValue("num1",  check3.replace("_",""));
-    //     break;
-
-    //   case "num2":
-    //     for (let i = 0; i < listArr.length; i++) {
-    //       if (listArr[i] === "_") {
-    //         listArr[i] = temp[temp.length - 1];
-    //         break;
-    //       }
-    //     }
-    //     setListArr(listArr);
-    //     str = listArr.join("");
-    //     break;
-
-    //   case "num3":
-    //     for (let i = 0; i < listArr3.length; i++) {
-    //       if (listArr3[i] === "_") {
-    //         listArr3[i] = temp[temp.length - 1];
-    //         break;
-    //       }
-    //     }
-    //     setListArr3(listArr3);
-    //     str = listArr3.join("");
-    //     formik.setFieldValue("num3", str);
-    //     break;
-
-    //   case "num4":
-    //     for (let i = 0; i < listArr4.length; i++) {
-    //       if (listArr4[i] === "_") {
-    //         listArr4[i] = temp[temp.length - 1];
-    //         break;
-    //       }
-    //     }
-    //     setListArr4(listArr4);
-    //     str = listArr4.join("");
-    //     break;
-    // }
-
   };
 
-  //Handle Backspace Fucntion
+  /**
+   * * this fucntion handle BackSpace event
+   */
 
   const handleBackspace = (e) => {
     let check = e.target.selectionStart - 1;
@@ -114,79 +72,18 @@ function App() {
 
     listArr.splice(check, 1, "_");
     formik.setFieldValue(name, listArr.join(""));
-
-    // switch (e.target.name) {
-    //   case "num1":
-    
-    //     break;
-
-    //   case "num2":
-    //     if (!reg.test(listArr2[check])) {
-    //       check = check - 1;
-    //       if (!reg.test(listArr2[check])) {
-    //         check = check - 1;
-    //       }
-    //     }
-
-    //     listArr2.splice(check, 1, "_");
-    //     formik.setFieldValue("num2", listArr2.join(""));
-
-    //     break;
-
-    //   case "num3":
-    //     if (!reg.test(listArr3[check])) {
-    //       check = check - 1;
-    //       if (!reg.test(listArr3[check])) {
-    //         check = check - 1;
-    //       }
-    //     }
-
-    //     listArr3.splice(check, 1, "_");
-    //     formik.setFieldValue("num3", listArr3.join(""));
-
-    //     break;
-
-    //   case "num4":
-    //     if (!reg.test(listArr4[check])) {
-    //       check = check - 1;
-    //       if (!reg.test(listArr4[check])) {
-    //         check = check - 1;
-    //       }
-    //     }
-
-    //     listArr4.splice(check, 1, "_");
-    //     formik.setFieldValue("num4", listArr4.join(""));
-
-    //     break;
-    // }
-    // if (!reg.test(listArr[check])) {
-    //   check = check - 1;
-    //   if (!reg.test(listArr[check])) {
-    //     check = check - 1;
-    //   }
-    // }
-
-    // listArr.splice(check, 1, "_");
-    // formik.setFieldValue("num1", listArr.join(""));
-
-    //   const reg = /\d/;
-    //   let arr = [];
-
-    //   for (let i = 0; i < checkNum.length; i++) {
-    //     if (reg.test(checkNum.at(i))) {
-    //       arr.push(checkNum.at(i));
-    //     }
-    //   }
-    //   arr.splice(check - 1, 1);
-    //
-    //
   };
+
+  /**
+   * * to target the first "_" on click in the text field
+   * * and move the cursor to the first "_" in the text field
+   */
 
   const handleClick = (e) => {
     const check = e.target.selectionStart;
     const regex = /\d/;
     const name = e.target.name;
-    console.log('name: ', formik.values[name]);
+    console.log("name: ", formik.values[name]);
 
     if (!regex.test(formik.values[name].charAt(check - 1))) {
       e.target.setSelectionRange(
@@ -194,45 +91,12 @@ function App() {
         formik.values[name].indexOf("_")
       );
     }
-
-    // switch (e.target.name) {
-    //   case "num1":
-    //     if (!regex.test(formik.values.num1.charAt(check - 1))) {
-    //       e.target.setSelectionRange(
-    //         formik.values.num1.indexOf("_"),
-    //         formik.values.num1.indexOf("_")
-    //       );
-    //     }
-    //     break;
-    //   case "num2":
-    //     if (!regex.test(formik.values.num2.charAt(check - 1))) {
-    //       e.target.setSelectionRange(
-    //         formik.values.num2.indexOf("_"),
-    //         formik.values.num2.indexOf("_")
-    //       );
-    //     }
-    //     break;
-    //   case "num3":
-    //     if (!regex.test(formik.values.num3.charAt(check - 1))) {
-    //       e.target.setSelectionRange(
-    //         formik.values.num3.indexOf("_"),
-    //         formik.values.num3.indexOf("_")
-    //       );
-    //     }
-    //     break;
-
-    //   case "num4":
-    //     if (!regex.test(formik.values.num4.charAt(check - 1))) {
-    //       e.target.setSelectionRange(
-    //         formik.values.num4.indexOf("_"),
-    //         formik.values.num4.indexOf("_")
-    //       );
-    //     }
-    //     break;
-    // }
-
-
   };
+
+  /**
+   * * does the same thing as fucntion above
+   * * but for the changes made in formik.values
+   */
 
   useEffect(() => {
     document
@@ -241,7 +105,6 @@ function App() {
         formik.values[fieldName].indexOf("_"),
         formik.values[fieldName].indexOf("_")
       );
-
   }, [
     formik.values.num1,
     formik.values.num2,
@@ -249,199 +112,48 @@ function App() {
     formik.values.num4,
   ]);
 
-  // document.getElementById("num3").addEventListener("keyup", (e) => {
-  //
-  // });
-
   return (
     <div className="App">
       <form onSubmit={formik.handleSubmit}>
-        <div>
-          <span>Number 1</span>
-          <input
-            type="text"
-            name="num1"
-            id="num1"
-            onChange={handleChange}
-            value={formik.values.num1}
-            placeholder="(__)-____-____"
-            onClick={handleClick}
-            onKeyDown={(e) => {
-              if (e.key === "Backspace") {
-                e.preventDefault();
-                handleBackspace(e);
-              }
-            }}
-            onFocus={(e) => {
+        <div className="form-main">
+          {data.map((item, index) => {
+            const { name, listarr, placeHolde } = item;
 
-              setFieldName('num1');
-              if (!e.target.value) {
-                formik.setFieldValue("num1", "(__)-____-____");
-              }
-              if (!formik.values.num1) {
-                setListArr([
-                  "(",
-                  "_",
-                  "_",
-                  ")",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                ]);
-              } else {
-                formik.setFieldValue("num1", formik.values.num1);
-                setListArr(formik.values.num1.split(""));
-              }
-            }}
-          />
+            return (
+              <div key={index} className="form-field">
+                <span>{name}</span>
+                <input
+                  type="text"
+                  name={name}
+                  id={name}
+                  onChange={handleChange}
+                  value={formik.values[name]}
+                  placeholder={placeHolde}
+                  onClick={handleClick}
+                  onKeyDown={(e) => {
+                    if (e.key === "Backspace") {
+                      e.preventDefault();
+                      handleBackspace(e);
+                    }
+                  }}
+                  onFocus={(e) => {
+                    setFieldName(name);
+                    if (!e.target.value) {
+                      formik.setFieldValue(name,placeHolde);
+                    }
+                    if (!formik.values[name]) {
+                      setListArr(listarr);
+                    } else {
+                      formik.setFieldValue(name, formik.values[name]);
+                      setListArr(formik.values[name].split(""));
+                    }
+                  }}
+                />
+              </div>
+            );
+          })}
         </div>
-        <div>
-          <span>Number 2</span>
-          <input
-            type="text"
-            name="num2"
-            id="num2"
-            value={formik.values.num2}
-            onChange={handleChange}
-            onClick={handleClick}
-            onFocus={(e) => {
-              setFieldName('num2');
-              if (!e.target.value) {
-                formik.setFieldValue("num2", "+__-__________");
-              }
-              if (!formik.values.num2) {
-                setListArr([
-                  "+",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                ]);
-              } else {
-                formik.setFieldValue("num2", formik.values.num2);
-                setListArr(formik.values.num2.split(""));
-              }
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Backspace") {
-                e.preventDefault();
-                handleBackspace(e);
-              }
-            }}
-            placeholder="+__-__________"
-          />
-        </div>
-        <div>
-          <span>Number 3</span>
-          <input
-            type="text"
-            name="num3"
-            id="num3"
-            value={formik.values.num3}
-            onChange={handleChange}
-            onClick={handleClick}
-            onFocus={(e) => {
-              setFieldName('num3');
-              if (!e.target.value) {
-                formik.setFieldValue("num3", "(___)-___-____");
-              }
-
-              if (!formik.values.num3) {
-                setListArr([
-                  "(",
-                  "_",
-                  "_",
-                  "_",
-                  ")",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                ]);
-              } else {
-                formik.setFieldValue("num3", formik.values.num3);
-                setListArr(formik.values.num3.split(""));
-              }
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Backspace") {
-                e.preventDefault();
-                handleBackspace(e);
-              }
-            }}
-            placeholder="(___)-___-____"
-          />
-        </div>
-        <div>
-          <span>Number 4</span>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="num4"
-            id="num4"
-            value={formik.values.num4}
-            onClick={handleClick}
-            onFocus={(e) => {
-              setFieldName('num4');
-              if (!e.target.value) {
-                formik.setFieldValue("num4", "+___-__-__-______");
-              }
-
-              if (!formik.values.num4) {
-                setListArr([
-                  "+",
-                  "_",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "-",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                  "_",
-                ]);
-              } else {
-                formik.setFieldValue("num4", formik.values.num4);
-                setListArr(formik.values.num4.split(""));
-              }
-            }}
-            placeholder="+___-__-__-______"
-            onKeyDown={(e) => {
-              if (e.key === "Backspace") {
-                e.preventDefault();
-                handleBackspace(e);
-              }
-            }}
-          />
-        </div>
+      
         <button type="submit">submit</button>
       </form>
     </div>
